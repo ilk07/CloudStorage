90b3d978365d5033eb45087aa82bd04fab45b1ef
ilk07 <ilk07@inbox.ru>
2023-06-09 04:27:47 +0300

    replace annotation component to configuration

--- src/main/java/com/hw/cloudstorage/config/UserFileConfig.java
+++ src/main/java/com/hw/cloudstorage/config/UserFileConfig.java
@@ -4,9 +4,8 @@ package com.hw.cloudstorage.config;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
@@ -18,7 +17,7 @@ import java.util.Set;
 @Getter
 @Setter
 @ToString
-@Component
+@Configuration
 @ConfigurationProperties("files")
 public class UserFileConfig {
     private Set<String> allowTypes;

3e7a01a91a31c814faadb82f807ff555af4277cd
ilk07 <ilk07@inbox.ru>
2023-06-10 03:23:22 +0300

    replaced the Optional class with FileEntity, findUserFileNameSizeByIdAndStatus_Named method Object type changed to FileEntityNameSize

--- src/main/java/com/hw/cloudstorage/repositories/FileRepository.java
+++ src/main/java/com/hw/cloudstorage/repositories/FileRepository.java
@@ -1,6 +1,7 @@
 package com.hw.cloudstorage.repositories;
 
-import com.hw.cloudstorage.dto.FileEntityNameSizeDto;
+
+import com.hw.cloudstorage.model.FileEntityNameSize;
 import com.hw.cloudstorage.model.entity.FileEntity;
 import com.hw.cloudstorage.model.entity.User;
 import com.hw.cloudstorage.model.enums.Status;
@@ -9,15 +10,14 @@ import org.springframework.data.jpa.repository.Query;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 
 @Transactional
 public interface FileRepository extends JpaRepository<FileEntity, Long> {
 
-    Optional<FileEntity> findByUserAndUploadNameAndStatus(User user, String uploadName, Status status);
+    FileEntity findByUserAndUploadNameAndStatus(User user, String uploadName, Status status);
 
     int countAllByUserAndUploadNameAndStatus(User user, String uploadName, Status status);
 
     @Query(nativeQuery = true)
-    List<FileEntityNameSizeDto> findUserFileNameSizeDtoByIdAndStatus_Named(Long userId, String status, int limit);
+    List<FileEntityNameSize> findUserFileNameSizeByIdAndStatus_Named(Long userId, String status, int limit);
 }

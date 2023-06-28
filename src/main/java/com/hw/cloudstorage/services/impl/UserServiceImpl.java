3b39c12ef4b51bd0066bc405ecfec744e9aa65b8
ilk07 <ilk07@inbox.ru>
2023-06-28 22:19:06 +0300

    added method getUser override, updated import

--- src/main/java/com/hw/cloudstorage/services/impl/UserServiceImpl.java
+++ src/main/java/com/hw/cloudstorage/services/impl/UserServiceImpl.java
@@ -1,32 +1,40 @@
 package com.hw.cloudstorage.services.impl;
 
 import com.hw.cloudstorage.model.entity.User;
-import com.hw.cloudstorage.repositories.RoleRepository;
 import com.hw.cloudstorage.repositories.UserRepository;
 import com.hw.cloudstorage.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.security.Principal;
 
 @Service
 public class UserServiceImpl implements UserService {
+
     private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
     }
 
     @Override
     public User findByUsername(String username) {
-        User user = userRepository.findByUsername(username);
-        return user;
+        return userRepository.findByUsername(username).orElse(null);
     }
 
     @Override
     public User findById(Long id) {
-        User user = userRepository.findById(id).orElse(null);
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User getUser(Principal principal) {
+        User user = findByUsername(principal.getName());
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found : " + principal.getName());
+        }
         return user;
     }
 }

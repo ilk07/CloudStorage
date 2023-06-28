9a328843f0c5b5d31304a34a228a997609fe0ff1
ilk07 <ilk07@inbox.ru>
2023-06-28 21:25:17 +0300

    optimized import

--- src/main/java/com/hw/cloudstorage/model/AuthenticationToken.java
+++ src/main/java/com/hw/cloudstorage/model/AuthenticationToken.java
@@ -1,7 +1,10 @@
 package com.hw.cloudstorage.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter

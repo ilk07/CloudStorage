59849f8182595cf696c3e54f5d2ff7d233fe8b2a
ilk07 <ilk07@inbox.ru>
2023-06-28 22:07:59 +0300

    update RedisConfig with RedisStandaloneConfiguration and settings from redis properties class

--- src/main/java/com/hw/cloudstorage/config/RedisConfig.java
+++ src/main/java/com/hw/cloudstorage/config/RedisConfig.java
@@ -1,10 +1,14 @@
 package com.hw.cloudstorage.config;
 
-import com.hw.cloudstorage.model.entity.BlockedToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
@@ -12,10 +16,25 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableConfigurationProperties(RedisProperties.class)
 @EnableRedisRepositories
 public class RedisConfig {
+    @Qualifier("redisProperties")
+    @Autowired
+    RedisProperties redisProperties;
+
     @Bean
-    public RedisTemplate<String, BlockedToken> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, BlockedToken> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
+    @Primary
+    RedisConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisProperties.getHost());
+        config.setPort(redisProperties.getPort());
+        config.setPassword(redisProperties.getPassword());
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
+
 }

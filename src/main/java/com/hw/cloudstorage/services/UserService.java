603eb94e2d3b441b8358172ddaf58f0ac3d06388
ilk07 <ilk07@inbox.ru>
2023-06-28 22:18:35 +0300

    added method getUser, updated import

--- src/main/java/com/hw/cloudstorage/services/UserService.java
+++ src/main/java/com/hw/cloudstorage/services/UserService.java
@@ -3,9 +3,11 @@ package com.hw.cloudstorage.services;
 
 import com.hw.cloudstorage.model.entity.User;
 
-import java.util.List;
+import java.security.Principal;
 
 public interface UserService {
     User findByUsername(String username);
     User findById(Long id);
+
+    User getUser(Principal principal);
 }

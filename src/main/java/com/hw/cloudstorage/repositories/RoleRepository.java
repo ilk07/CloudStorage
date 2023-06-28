532bbfcfc46958d379aa75d7ba9e7712b4902cb3
ilk07 <ilk07@inbox.ru>
2023-06-28 22:09:18 +0300

    add findByName method

--- src/main/java/com/hw/cloudstorage/repositories/RoleRepository.java
+++ src/main/java/com/hw/cloudstorage/repositories/RoleRepository.java
@@ -4,5 +4,4 @@ import com.hw.cloudstorage.model.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
 }

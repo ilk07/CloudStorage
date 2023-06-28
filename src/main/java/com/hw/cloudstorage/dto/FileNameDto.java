24c648e039b38d4328aeba8af8b5d1b36b95833a
ilk07 <ilk07@inbox.ru>
2023-06-28 21:48:06 +0300

    update import

--- src/main/java/com/hw/cloudstorage/dto/FileNameDto.java
+++ src/main/java/com/hw/cloudstorage/dto/FileNameDto.java
@@ -1,6 +1,9 @@
 package com.hw.cloudstorage.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 
